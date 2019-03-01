79be75d4e028bd491a77e71e3bc3f9e7380097ce
Gregory Sanders <grysar@zort.net>
2019-03-01 18:04:48 -0500

    Retesting FPDSimporter as lead in to service contract inventory.

--- FPDSimport.r
+++ FPDSimport.r
@@ -2,11 +2,27 @@
 source("SQLimportTools.r")
 library(tidyverse)
 
-#******Importing into Errorlogging.FPDSviolatesConstraint Classic
-#Match up Errorlogging.FPDSviolatesType to Errorlogging.FPDSviolatesConstraint
-OriginTableType.df<-read_create_table("ErrorLogging_FPDSviolatesType.txt")
-DestTableType.df<-read_create_table("ErrorLogging_FPDSviolatesConstraint.txt")
-OriginTableType.df<-translate_name(OriginTableType.df)
+
+#******Importing into Errorlogging.FPDSviolatesConstraint Beta
+#Creating Stage 2 database from Stage 1 database and Final Destination
+#No Need to rerun this, but good to have the code for the future
+OriginBetaTableType.df<-read_create_table("ErrorLogging_FPDSbetaViolatesType.txt")
+OriginBetaTableType.df<-translate_name(OriginBetaTableType.df)
+FinalTableType.df<-read_create_table("Contract_FPDS.txt")
+NewConstraintTableType.df<-merge_source_and_csis_name_tables(OriginBetaTableType.df,FinalTableType.df,
+                                                             drop_unmatched=FALSE)
+
+
+#Create a list of variables to be copied into a query to create the stage 2 database.
+Create_Constraint_List<-paste(NewConstraintTableType.df$CSISvariableName,
+                              NewConstraintTableType.df$CSISvariableType,
+                              NewConstraintTableType.df$CSISnullable,",")
+write(Create_Constraint_List,
+      file="ImportAids\\Starter_ErrorLogging_FPDSbetaViolatesConstraint.txt")
+
+debug(create_foreign_key_assigments)
+create_foreign_key_assigments("ErrorLogging","FPDSbetaViolatesConstraint")
+
 MergeType.df<-merge_source_and_csis_name_tables(OriginTableType.df,DestTableType.df)
 
 
@@ -19,7 +35,7 @@ MergeType.df$column<-substring(MergeType.df$column,2,nchar(MergeType.df$column)-
 # undebug(create_try_converts)
 TryConvertList<-create_try_converts(MergeType.df,"Errorlogging","FPDSviolatesType"
   ,IncludeAlters=FALSE)
-write(TryConvertList,"FPDStryConvertList.txt")
+write(TryConvertList,"ImportAids\\FPDStryConvertList.txt")
 
 #Transfer from Errorlogging.FPDSviolatesType to Errorlogging.FPDSviolatesConstraint
 InsertList<-create_insert(MergeType.df,
@@ -28,57 +44,8 @@ InsertList<-create_insert(MergeType.df,
   "ErrorLogging",
   "FPDSviolatesConstraint",
   DateType=120)
-write(InsertList,"Insert.txt")
-write(create_csis_dates("Contract","FPDS"),"CSISdates.txt")
-
-#******Importing into Errorlogging.FPDSviolatesConstraint Beta
-#Creating Errorlogging.FPDSbetaviolatesConstraint from Errorlogging.FPDSbetaviolatesType and Contract.FPDS
-#No Need to rerun this, but good to have the code for the future
-OriginBetaTableType.df<-read_create_table("ErrorLogging_FPDSbetaViolatesType.txt")
-OriginBetaTableType.df<-translate_name(OriginBetaTableType.df)
-FinalTableType.df<-read_create_table("Contract_FPDS.txt")
-NewConstraintTableType.df<-merge_source_and_csis_name_tables(OriginBetaTableType.df,FinalTableType.df,
-  drop_unmatched=FALSE)
-Create_Constraint_List<-paste(NewConstraintTableType.df$CSISvariableName,
-  NewConstraintTableType.df$CSISvariableType,
-  NewConstraintTableType.df$CSISnullable,",")
-write(Create_Constraint_List,
-  file="ImportAids\\Starter_ErrorLogging_FPDSbetaViolatesConstraint.txt")
-
-debug(create_foreign_key_assigments)
-create_foreign_key_assigments("ErrorLogging","FPDSbetaViolatesConstraint")
-#Match up Errorlogging.FPDSviolatesType to Errorlogging.FPDSviolatesConstraint
-OriginTableType.df<-read_create_table("ErrorLogging_FPDSbetaViolatesType.txt")
-DestTableType.df<-read_create_table("ErrorLogging_FPDSbetaViolatesConstraint.txt")
-OriginTableType.df<-translate_name(OriginTableType.df)
-MergeType.df<-merge_source_and_csis_name_tables(OriginTableType.df,DestTableType.df)
-
-
-#Manual fixes
-#[small_business_competitiveness_demonstration _program]
-#[historically_underutilized_business_zone _hubzone_firm]
-
-
-  #Join up the files
-MergeType.df$column<-MergeType.df$SourceVariableName
-MergeType.df$column<-substring(MergeType.df$column,2,nchar(MergeType.df$column)-1)
-  
-
-#Create Try Convert
-# undebug(create_try_converts)
-TryConvertList<-create_try_converts(MergeType.df,"Errorlogging","FPDSbetaViolatesType"
-                                    ,IncludeAlters=FALSE)
-write(TryConvertList,"FPDSbetatryConvertList.txt")
-
-#Transfer from Errorlogging.FPDSviolatesType to Errorlogging.FPDSviolatesConstraint
-InsertList<-create_insert(MergeType.df,
-             "ErrorLogging",
-             "FPDSbetaViolatesType",
-             "ErrorLogging",
-             "FPDSbetaViolatesConstraint",
-             DateType=101)
-write(InsertList,"BetaInsert.txt")
-write(create_csis_dates("Contract","FPDS"),"CSISdates.txt")
+write(InsertList,"ImportAids\\Insert.txt")
+write(create_csis_dates("Contract","FPDS"),"ImportAids//CSISdates.txt")
 
 #******Importing into Contract.FPDS 
 #Match up Errorlogging.FPDSviolatesConstraint to Contract.FPDS 
